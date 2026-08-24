a845566def916b3c22f24e83c45b6c864801da24
jaurim007-ui <jaurim007@gmail.com>
2026-08-25 00:10:18 +0900

    Refresh service worker after icon fix

--- sw.js
+++ sw.js
@@ -1,14 +1,6 @@
-const VERSION='sungsim-pwa-v4';
-self.addEventListener('install',event=>{
-  self.skipWaiting();
-});
+const VERSION='sungsim-pwa-v5';
+self.addEventListener('install',event=>{self.skipWaiting();});
 self.addEventListener('activate',event=>{
-  event.waitUntil(
-    caches.keys()
-      .then(keys=>Promise.all(keys.map(key=>caches.delete(key))))
-      .then(()=>self.clients.claim())
-  );
+  event.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>caches.delete(k)))).then(()=>self.clients.claim()));
 });
-self.addEventListener('fetch',event=>{
-  // 네트워크 요청을 그대로 사용합니다. 이 fetch 핸들러는 PWA 설치 요건 호환성을 위한 최소 구성입니다.
-});
+self.addEventListener('fetch',event=>{});
